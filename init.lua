68cfb340e679121dc7ce497acaed1c5ff40e7a83
sortedcord <mail@adityagupta.dev>
2025-08-23 23:45:49 +0530

    fix: osc52 clipboard

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.clipboard = "osc52"
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    vim.opt.clipboard = "unnamedplus"
+  end,
+})
